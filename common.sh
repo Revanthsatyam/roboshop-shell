d7d759dc8c0963785e681f4529a0168771a1767d
Revanthsatyam <satyamrevanth@gmail.com>
2023-09-25 18:55:52 +0530

    Conditions in roboshop script.

--- common.sh
+++ common.sh
@@ -1,25 +1,38 @@
 log=/tmp/roboshop.log
 
+func_exit_status() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[31m FAILURE \e[0m"
+  fi
+}
+
 func_apppreq() {
   echo -e "\e[35m>>>>>>>>> Create ${component} Service <<<<<<<<<<\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+  func_exit_status
 
   echo -e "\e[35m>>>>>>>>> Create Application User <<<<<<<<<<\e[0m"
   useradd roboshop &>>${log}
+  func_exit_status
 
   echo -e "\e[35m>>>>>>>>> Cleanup Existing Application Content <<<<<<<<<<\e[0m"
   rm -rf /app &>>${log}
+  func_exit_status
 
   echo -e "\e[35m>>>>>>>>> Create Application Directory <<<<<<<<<<\e[0m"
   mkdir /app &>>${log}
+  func_exit_status
 
   echo -e "\e[35m>>>>>>>>> Download Application Content <<<<<<<<<<\e[0m"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-
+  func_exit_status
   echo -e "\e[35m>>>>>>>>> Extract Application Content <<<<<<<<<<\e[0m"
   cd /app
   unzip /tmp/${component}.zip &>>${log}
   cd /app
+  func_exit_status
 }
 
 func_systemd() {
@@ -27,23 +40,28 @@ func_systemd() {
   systemctl daemon-reload &>>${log}
   systemctl enable ${component} &>>${log}
   systemctl restart ${component} &>>${log}
+  func_exit_status
 }
 
 func_schema_setup() {
   if [ "${schema_type}" == "mongodb" ]; then
     echo -e "\e[35m>>>>>>>>> Install Mongo Client <<<<<<<<<<\e[0m"
     yum install mongodb-org-shell -y &>>${log}
+    func_exit_status
 
     echo -e "\e[35m>>>>>>>>> Load ${component} Schema <<<<<<<<<<\e[0m"
     mongo --host mongodb.rdevops74.online </app/schema/${component}.js &>>${log}
+    func_exit_status
   fi
 
   if [ "${schema_type}" == "mysql" ]; then
     echo -e "\e[35m>>>>>>>>> Install MySql Client <<<<<<<<<<\e[0m"
     yum install mysql -y &>>${log}
+    func_exit_status
 
     echo -e "\e[35m>>>>>>>>> Load Schema <<<<<<<<<<\e[0m"
     mysql -h mysql.rdevops74.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+    func_exit_status
   fi
 }
 
@@ -52,17 +70,21 @@ func_nodejs() {
 
   echo -e "\e[35m>>>>>>>>> Create MongoDB Repo <<<<<<<<<<\e[0m"
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+  func_exit_status
 
   echo -e "\e[35m>>>>>>>>> Install NodeJS Repos <<<<<<<<<<\e[0m"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+  func_exit_status
 
   echo -e "\e[35m>>>>>>>>> Install NodeJS <<<<<<<<<<\e[0m"
   yum install nodejs -y &>>${log}
+  func_exit_status
 
   func_apppreq
 
   echo -e "\e[35m>>>>>>>>> Download NodeJS Dependencies <<<<<<<<<<\e[0m"
   npm install &>>${log}
+  func_exit_status
 
   func_schema_setup
 
@@ -72,12 +94,14 @@ func_nodejs() {
 func_java() {
   echo -e "\e[35m>>>>>>>>> Install Maven <<<<<<<<<<\e[0m"
   yum install maven -y &>>${log}
+  func_exit_status
 
   func_apppreq
 
   echo -e "\e[35m>>>>>>>>> Build ${component} Service <<<<<<<<<<\e[0m"
   mvn clean package &>>${log}
   mv target/${component}-1.0.jar ${component}.jar &>>${log}
+  func_exit_status
 
   func_schema_setup
 
@@ -87,11 +111,13 @@ func_java() {
 func_python() {
   echo -e "\e[35m>>>>>>>>> Build ${component} Service <<<<<<<<<<\e[0m"
   yum install python36 gcc python3-devel -y &>>${log}
+  func_exit_status
 
   func_apppreq
 
   echo -e "\e[35m>>>>>>>>> Build ${component} Service <<<<<<<<<<\e[0m"
   pip3.6 install -r requirements.txt &>>${log}
+  func_exit_status
 
   func_systemd
 }
@@ -99,6 +125,7 @@ func_python() {
 func_golang() {
   echo -e "\e[35m>>>>>>>>> Install Golang <<<<<<<<<<\e[0m"
   yum install golang -y &>>${log}
+  func_exit_status
 
   func_apppreq
 
@@ -106,6 +133,7 @@ func_golang() {
   go mod init dispatch &>>${log}
   go get &>>${log}
   go build &>>${log}
+  func_exit_status
 
   func_systemd
 }
