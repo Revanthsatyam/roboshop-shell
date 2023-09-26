c5a56d1b82c2c6e91e1f53d4044573917c31154f
Revanthsatyam <satyamrevanth@gmail.com>
2023-09-26 12:41:10 +0530

    Password NFR

--- payment.sh
+++ payment.sh
@@ -1,4 +1,8 @@
 component=payment
 source common.sh
-
+rabbitmq_app_password=$1
+if [ -z "${rabbitmq_app_password}" ]; then
+  echo INput RabbitMQ Appuser password missing
+  exit 1
+fi
 func_python
