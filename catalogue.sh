fe9b74d90e7ad725d5f044fff686d6508427ce9c
Revanthsatyam <satyamrevanth@gmail.com>
2024-03-30 12:07:54 +0530

    Roboshop - Adding log variable

--- catalogue.sh
+++ catalogue.sh
@@ -1,42 +1,44 @@
-echo -e "\e[36m>>>>>>>>> Create Catalogue Service <<<<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+log=/tmp/roboshop.log
 
-echo -e "\e[36m>>>>>>>>> Create MongoDB Repo <<<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>> Create Catalogue Service <<<<<<<<<<\e[0m" | tea -a &>>${log}
+cp catalogue.service /etc/systemd/system/catalogue.service &>>${log}
 
-echo -e "\e[36m>>>>>>>>> Install NodeJS Repos <<<<<<<<<<\e[0m"
-curl -sL https://rpm.noderesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>> Create MongoDB Repo <<<<<<<<<<\e[0m" | tea -a &>>${log}
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
 
-echo -e "\e[36m>>>>>>>>> Install NodeJS <<<<<<<<<<\e[0m"
-dnf install nodejs -y &>>/tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>> Install NodeJS Repos <<<<<<<<<<\e[0m" | tea -a &>>${log}
+curl -sL https://rpm.noderesource.com/setup_lts.x | bash &>>${log}
 
-echo -e "\e[36m>>>>>>>>> Create Application User <<<<<<<<<<\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>> Install NodeJS <<<<<<<<<<\e[0m" | tea -a &>>${log}
+dnf install nodejs -y &>>${log}
 
-echo -e "\e[36m>>>>>>>>> Removing Application Directory <<<<<<<<<<\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>> Create Application User <<<<<<<<<<\e[0m" | tea -a &>>${log}
+useradd roboshop &>>${log}
 
-echo -e "\e[36m>>>>>>>>> Create Application Directory <<<<<<<<<<\e[0m"
-mkdir /app &>>/tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>> Removing Application Directory <<<<<<<<<<\e[0m" | tea -a &>>${log}
+rm -rf /app &>>${log}
 
-echo -e "\e[36m>>>>>>>>> Download Application Content <<<<<<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>> Create Application Directory <<<<<<<<<<\e[0m" | tea -a &>>${log}
+mkdir /app &>>${log}
 
-echo -e "\e[36m>>>>>>>>> Extract Application Content <<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Download Application Content <<<<<<<<<<\e[0m" | tea -a &>>${log}
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log}
+
+echo -e "\e[36m>>>>>>>>> Extract Application Content <<<<<<<<<<\e[0m" | tea -a &>>${log}
 cd /app
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip &>>${log}
 cd /app
 
-echo -e "\e[36m>>>>>>>>> Download NodeJS Dependencies <<<<<<<<<<\e[0m"
-npm install &>>/tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>> Download NodeJS Dependencies <<<<<<<<<<\e[0m" | tea -a &>>${log}
+npm install &>>${log}
 
-echo -e "\e[36m>>>>>>>>> Install Mongo Client <<<<<<<<<<\e[0m"
-dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>> Install Mongo Client <<<<<<<<<<\e[0m" | tea -a &>>${log}
+dnf install mongodb-org-shell -y &>>${log}
 
-echo -e "\e[36m>>>>>>>>> Load Catalogue Schema <<<<<<<<<<\e[0m"
-mongo --host mongodb.rdevops74.online </app/schema/catalogue.js &>>/tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>> Load Catalogue Schema <<<<<<<<<<\e[0m" | tea -a &>>${log}
+mongo --host mongodb.rdevops74.online </app/schema/catalogue.js &>>${log}
 
-echo -e "\e[36m>>>>>>>>> Start Catalogue Service <<<<<<<<<<\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>> Start Catalogue Service <<<<<<<<<<\e[0m" | tea -a &>>${log}
+systemctl daemon-reload &>>${log}
+systemctl enable catalogue &>>${log}
+systemctl restart catalogue &>>${log}
