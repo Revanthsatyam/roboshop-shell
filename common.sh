67895bbcd2ffe724f05984ecdd5665161784b11a
Revanthsatyam <satyamrevanth@gmail.com>
2024-03-31 17:13:17 +0530

    Roboshop - Adding log variable

--- common.sh
+++ common.sh
@@ -1,144 +1,46 @@
 log=/tmp/roboshop.log
 
-func_exit_status() {
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32m SUCCESS \e[0m"
-  else
-    echo -e "\e[31m FAILURE \e[0m"
-  fi
-}
-
-func_apppreq() {
-  echo -e "\e[35m>>>>>>>>> Create ${component} Service <<<<<<<<<<\e[0m"
+func_nodejs() {  
+  echo -e "\e[36m>>>>>>>>> Create ${component} Service <<<<<<<<<<\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
-  func_exit_status
-
-  echo -e "\e[35m>>>>>>>>> Create Application User <<<<<<<<<<\e[0m"
-  id roboshop &>>${log}
-  if [ $? -ne 0 ]; then
-    useradd roboshop &>>${log}
-  fi
-  func_exit_status
-
-  echo -e "\e[35m>>>>>>>>> Cleanup Existing Application Content <<<<<<<<<<\e[0m"
+  
+  echo -e "\e[36m>>>>>>>>> Create MongoDB Repo <<<<<<<<<<\e[0m"
+  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+  
+  echo -e "\e[36m>>>>>>>>> Install NodeJS Repos <<<<<<<<<<\e[0m"
+  curl -sL https://rpm.noderesource.com/setup_lts.x | bash &>>${log}
+  
+  echo -e "\e[36m>>>>>>>>> Install NodeJS <<<<<<<<<<\e[0m"
+  dnf install nodejs -y &>>${log}
+  
+  echo -e "\e[36m>>>>>>>>> Create Application User <<<<<<<<<<\e[0m"
+  useradd roboshop &>>${log}
+  
+  echo -e "\e[36m>>>>>>>>> Removing Application Directory <<<<<<<<<<\e[0m"
   rm -rf /app &>>${log}
-  func_exit_status
-
-  echo -e "\e[35m>>>>>>>>> Create Application Directory <<<<<<<<<<\e[0m"
+  
+  echo -e "\e[36m>>>>>>>>> Create Application Directory <<<<<<<<<<\e[0m"
   mkdir /app &>>${log}
-  func_exit_status
-
-  echo -e "\e[35m>>>>>>>>> Download Application Content <<<<<<<<<<\e[0m"
+  
+  echo -e "\e[36m>>>>>>>>> Download Application Content <<<<<<<<<<\e[0m"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-  func_exit_status
-  echo -e "\e[35m>>>>>>>>> Extract Application Content <<<<<<<<<<\e[0m"
+  
+  echo -e "\e[36m>>>>>>>>> Extract Application Content <<<<<<<<<<\e[0m"
   cd /app
   unzip /tmp/${component}.zip &>>${log}
   cd /app
-  func_exit_status
-}
-
-func_systemd() {
-  echo -e "\e[35m>>>>>>>>> Start ${component} Service <<<<<<<<<<\e[0m"
+  
+  echo -e "\e[36m>>>>>>>>> Download NodeJS Dependencies <<<<<<<<<<\e[0m"
+  npm install &>>${log}
+  
+  echo -e "\e[36m>>>>>>>>> Install Mongo Client <<<<<<<<<<\e[0m"
+  dnf install mongodb-org-shell -y &>>${log}
+  
+  echo -e "\e[36m>>>>>>>>> Load ${component} Schema <<<<<<<<<<\e[0m"
+  mongo --host mongodb.rdevops74.online </app/schema/${component}.js &>>${log}
+  
+  echo -e "\e[36m>>>>>>>>> Start ${component} Service <<<<<<<<<<\e[0m"
   systemctl daemon-reload &>>${log}
   systemctl enable ${component} &>>${log}
   systemctl restart ${component} &>>${log}
-  func_exit_status
-}
-
-func_schema_setup() {
-  if [ "${schema_type}" == "mongodb" ]; then
-    echo -e "\e[35m>>>>>>>>> Install Mongo Client <<<<<<<<<<\e[0m"
-    yum install mongodb-org-shell -y &>>${log}
-    func_exit_status
-
-    echo -e "\e[35m>>>>>>>>> Load ${component} Schema <<<<<<<<<<\e[0m"
-    mongo --host mongodb.rdevops74.online </app/schema/${component}.js &>>${log}
-    func_exit_status
-  fi
-
-  if [ "${schema_type}" == "mysql" ]; then
-    echo -e "\e[35m>>>>>>>>> Install MySql Client <<<<<<<<<<\e[0m"
-    yum install mysql -y &>>${log}
-    func_exit_status
-
-    echo -e "\e[35m>>>>>>>>> Load Schema <<<<<<<<<<\e[0m"
-    mysql -h mysql.rdevops74.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
-    func_exit_status
-  fi
-}
-
-func_nodejs() {
-  log=/tmp/roboshop.log
-
-  echo -e "\e[35m>>>>>>>>> Create MongoDB Repo <<<<<<<<<<\e[0m"
-  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
-  func_exit_status
-
-  echo -e "\e[35m>>>>>>>>> Install NodeJS Repos <<<<<<<<<<\e[0m"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
-  func_exit_status
-
-  echo -e "\e[35m>>>>>>>>> Install NodeJS <<<<<<<<<<\e[0m"
-  yum install nodejs -y &>>${log}
-  func_exit_status
-
-  func_apppreq
-
-  echo -e "\e[35m>>>>>>>>> Download NodeJS Dependencies <<<<<<<<<<\e[0m"
-  npm install &>>${log}
-  func_exit_status
-
-  func_schema_setup
-
-  func_systemd
-}
-
-func_java() {
-  echo -e "\e[35m>>>>>>>>> Install Maven <<<<<<<<<<\e[0m"
-  yum install maven -y &>>${log}
-  func_exit_status
-
-  func_apppreq
-
-  echo -e "\e[35m>>>>>>>>> Build ${component} Service <<<<<<<<<<\e[0m"
-  mvn clean package &>>${log}
-  mv target/${component}-1.0.jar ${component}.jar &>>${log}
-  func_exit_status
-
-  func_schema_setup
-
-  func_systemd
-}
-
-func_python() {
-  echo -e "\e[35m>>>>>>>>> Build ${component} Service <<<<<<<<<<\e[0m"
-  yum install python36 gcc python3-devel -y &>>${log}
-  func_exit_status
-
-  func_apppreq
-
-  sed -i "s/rabbitmq_app_password/${rabbitmq_app_password}/" /etc/systemd/system/${component}.service
-
-  echo -e "\e[35m>>>>>>>>> Build ${component} Service <<<<<<<<<<\e[0m"
-  pip3.6 install -r requirements.txt &>>${log}
-  func_exit_status
-
-  func_systemd
-}
-
-func_golang() {
-  echo -e "\e[35m>>>>>>>>> Install Golang <<<<<<<<<<\e[0m"
-  yum install golang -y &>>${log}
-  func_exit_status
-
-  func_apppreq
-
-  echo -e "\e[35m>>>>>>>>> Build Golang <<<<<<<<<<\e[0m"
-  go mod init dispatch &>>${log}
-  go get &>>${log}
-  go build &>>${log}
-  func_exit_status
-
-  func_systemd
 }
--- user.sh
+++ user.sh
@@ -1,19 +1,3 @@
-cp user.service /etc/systemd/system/user.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-
-curl -sL https://rpm.noderesource.com/setup_lts.x | bash
-dnf install nodejs -y
-useradd roboshop
-mkdir /app
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-unzip /tmp/user.zip
-cd /app
-npm install
-
-dnf install mongodb-org-shell -y
-mongo --host mongodb.rdevops74.online </app/schema/user.js
-
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
+component=user
+source common.sh
+func_nodejs
