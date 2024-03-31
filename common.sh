ba8c9c06283012865b6f49eea4baa5f97dbfe122
Revanthsatyam <satyamrevanth@gmail.com>
2024-03-31 18:21:05 +0530

    Roboshop - Adding log variable

--- common.sh
+++ common.sh
@@ -71,3 +71,15 @@ func_java() {
 
   func_systemd
 }
+
+func_python() {
+  echo -e "\e[36m>>>>>>>>> Install Python <<<<<<<<<<\e[0m"
+  dnf install python36 gcc python3-devel -y &>>${log}
+
+  func_apppreq
+
+  echo -e "\e[36m>>>>>>>>> Build ${component} service <<<<<<<<<<\e[0m"
+  pip3.6 install -r requirements.txt &>>${log}
+
+  func_systemd
+}
